42d646926349189d473879219d4a5e6f07ce14ca
digitiveinc <admin@digitive.jp>
2026-03-25 09:08:49 +0900

    fix: specify login template path for production (registration/login.html not found)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     # ログアウトURLの設定
-	path('login/', auth_views.LoginView.as_view(), name='login'),
+	path('login/', auth_views.LoginView.as_view(template_name='app_folder/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
